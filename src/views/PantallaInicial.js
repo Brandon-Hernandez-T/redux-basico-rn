45bfad0628bb14a36baad745b3a05334e224fa1e
braniitorres <braniitorres@gmail.com>
2021-04-08 20:11:35 -0500

    :hammer: UPDATE: Se actualiza pantalla inicio
    Se carga la configuración de Redux para almacenar la información del formulario

--- src/views/PantallaInicial.js
+++ src/views/PantallaInicial.js
@@ -1,11 +1,63 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+/** ESTILOS */
+import EstilosGenerales from '../styles/EstilosGenerales';
+/** REDUX */
+import { useDispatch } from 'react-redux';
+import { saveData } from '../actions/Datos';
 
 const PantallaInicial = ({ navigation }) => {
 
-    return(
-        <View style={{ backgroundColor: 'red', }} >
-            <Text> Pantalla Inicial </Text>
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
+    /** REDUX VARIABLE */
+    const dispatch = useDispatch();
+
+
+    const almacenarEnRedux = () => {
+        //ALMACENAR EN REDUX
+        dispatch(saveData(nombre, apellido));
+    };
+
+    return (
+        <View style={EstilosGenerales.contenedorPincipal} >
+            <View style={EstilosGenerales.contenedorTitulo} >
+                <Text style={EstilosGenerales.textoTitulo} > Formulario para Redux </Text>
+            </View>
+
+            <View style={EstilosGenerales.contenedorFormulario} >
+                <View style={EstilosGenerales.espacioInputs} >
+                    <TextInput
+                        value={nombre}
+                        onChangeText={(texto) => setNombre(texto)}
+                        placeholder='   Nombre'
+                        style={EstilosGenerales.textInput}
+                    />
+                </View>
+                <View style={EstilosGenerales.espacioInputs} >
+                    <TextInput
+                        value={apellido}
+                        onChangeText={(texto) => setApellido(texto)}
+                        placeholder='Apellido'
+                        style={EstilosGenerales.textInput}
+                    />
+                </View>
+            </View>
+            <View style={EstilosGenerales.contenedorBotones} >
+                <View style={EstilosGenerales.boton} >
+                    <Button
+                        title="Guardar"
+                        onPress={() => almacenarEnRedux() }
+                    />
+                </View>
+                <View style={EstilosGenerales.boton} >
+                    <Button
+                        title="Info"
+                        onPress={() => navigation.navigate('Mostrar Información') }
+                        color='green'
+                    />
+                </View>
+            </View>
         </View>
     )
 };
